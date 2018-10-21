5d10a68b973df0bf76419267fc54617c253dd3b9
yzworker <15898193673@163.com>
2018-10-21 16:59:08 +0800

    个人信息

--- src/components/setting/index.vue
+++ src/components/setting/index.vue
@@ -1,8 +1,29 @@
 <template>
   <div>
     <el-form ref="form" label-width="80px">
-      <el-form-item prop="articleTitle" label="用户名">
+       <el-form-item prop="user" label="登录帐号">
+        <el-input v-model="userInfo.username" placeholder="昵称"></el-input>
+      </el-form-item>
+      <el-form-item prop="user" label="昵称">
         <el-input v-model="userInfo.displayName" placeholder="昵称"></el-input>
+      </el-form-item>
+       <el-form-item prop="user" label="性别">
+         <el-select v-model="userInfo.userSex" placeholder="性别">
+          <el-option label="男" value="男"></el-option>
+          <el-option label="女" value="女"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item prop="user" label="联系电话">
+        <el-input v-model="userInfo.userPhone" placeholder="联系电话"></el-input>
+      </el-form-item>
+      <el-form-item prop="user" label="邮箱">
+        <el-input v-model="userInfo.userEmail" placeholder="邮箱"></el-input>
+      </el-form-item>
+       <el-form-item prop="user" label="联系地址">
+        <el-input v-model="userInfo.userAddress" placeholder="联系地址"></el-input>
+      </el-form-item>
+       <el-form-item prop="user" label="个人描述">
+        <el-input type="textarea" v-model="userInfo.userDescription" placeholder="个人描述"></el-input>
       </el-form-item>
       <el-form-item prop="articleTitle" label="绑定方式">
         QQ
@@ -26,33 +47,30 @@
   </div>
 </template>
 <script>
-  export default {
-    name: 'Setting',
-    data () {
-      return {}
+export default {
+  name: "Setting",
+  data() {
+    return {};
+  },
+  async mounted() {
+    await this.$store.dispatch("CONNECT");
+  },
+  computed: {
+    userInfo() {
+      return this.$store.state.login.authUser;
     },
-    async mounted () {
-      await this.$store.dispatch('CONNECT')
-    },
-    computed: {
-      userInfo() {
-        return this.$store.state.login.authUser
-      },
-      connect() {
-        return this.$store.state.login.connect
-      }
-    },
-    methods: {
-      bindconnect(type){
-        this.$store.dispatch('BINDCONNECT', type)
-      },
-      unbindconnect(type){
-
-      }
-    },
-    components: {
+    connect() {
+      return this.$store.state.login.connect;
     }
-  }
+  },
+  methods: {
+    bindconnect(type) {
+      this.$store.dispatch("BINDCONNECT", type);
+    },
+    unbindconnect(type) {}
+  },
+  components: {}
+};
 </script>
 <style scoped lang="scss">
 </style>
